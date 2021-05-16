d57fca1119182d34550fe807ff990be7947beaea
kiber-bob <dn.ermak@list.ru>
2021-05-16 15:53:58 +0300

    Commit added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 30_000; // баланс на счету в копейках
+        int balance = 10_000; // баланс на счету в копейках
         int limit = 110_000; //  сумма пополнения в копейках
-        int balanceSummary = balance + limit;
-        System.out.println("balanceSummary: " + balanceSummary);
         int bonus;
         if (limit >= 100_000) {  // бонус в копейках
             bonus = (balance + limit) / 100;
         } else {
             bonus = 0;
         }
+        int balanceSummary = balance + limit + bonus;
+        System.out.println("balanceSummary: " + balanceSummary);
         System.out.println("bonus: " + bonus);
     }
 
